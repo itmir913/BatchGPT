990d9bfb21f1141655936f95cd0eced3231e7662
Miru <itmir913@gmail.com>
2025-01-04 23:26:29 +0900

    Cleanup

--- backend/job/migrations/0001_initial.py
+++ backend/job/migrations/0001_initial.py
@@ -9,7 +9,6 @@ import job.models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,9 @@
 const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../backend/dist', // 빌드 파일 경로를 Django 정적 파일 디렉토리로 지정
-  assetsDir: 'static',
-  devServer: {
-    proxy: 'http://127.0.0.1:8000', // Django 개발 서버와 연동 (필요 시)
-  },
+    transpileDependencies: true,
+    outputDir: '../backend/dist', // 빌드 파일 경로를 Django 정적 파일 디렉토리로 지정
+    assetsDir: 'static',
+    devServer: {
+        proxy: 'http://127.0.0.1:8000', // Django 개발 서버와 연동 (필요 시)
+    },
 });
