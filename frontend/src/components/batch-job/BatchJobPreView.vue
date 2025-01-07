af25725cb954e77aa94adfe40e3f6359858d22b9
Miru <itmir913@gmail.com>
2025-01-07 19:47:35 +0900

    previewData.CSV.selectedColumns 변수 명 변경이 반영되지 않았던 코드 수정

--- frontend/src/components/batch-job/BatchJobPreView.vue
+++ frontend/src/components/batch-job/BatchJobPreView.vue
@@ -217,7 +217,7 @@ export default {
       this.loadingState.configSave = true;
 
       // TODO CSV에서만 작동해야 한다.
-      if (Array.isArray(this.selectedColumns) && this.selectedColumns.length === 0) {
+      if (Array.isArray(this.previewData.CSV.selectedColumns) && this.previewData.CSV.selectedColumns.length === 0) {
         this.handleMessages("error", ERROR_MESSAGES.noColumn);
         this.loadingState.configSave = false;
         return;
