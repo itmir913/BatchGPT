02d5cb37eb60c78c52d98122f10153983927d98c
Miru <itmir913@gmail.com>
2025-01-30 06:27:22 +0900

    Update: build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sed -i 's/\r$//' entrypoint.sh
-
 IMAGE_NAME=itmir913/batchgpt
-TAG=latest
 
+TAG=v0.0.0
+docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME:$TAG --push .
+
+TAG=latest
 docker buildx build --platform linux/amd64,linux/arm64 -t $IMAGE_NAME:$TAG --push .
